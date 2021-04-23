fcb6562d96e05bfbd2ee4ab5f4c56b4598427c62
Robin Andersson <roban591@student.liu.se>
2021-04-23 13:53:33 +0200

    Move angular from devDependencies to dependencies in package.json. Remove django's static files serving and only use whitenoise.

--- server/server/urls.py
+++ server/server/urls.py
@@ -14,15 +14,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.conf.urls import url, include
-from django.conf import settings
-from django.conf.urls.static import static
-from django.urls import path
-
-from users import views
 
 urlpatterns = [
     url(r'^', include('users.urls')),
 ]
-
-urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-
