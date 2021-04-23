06900994d45b46e2121757b8f19c963a9b9355a1
Maya <somgh007@student.liu.se>
2021-04-23 16:58:43 +0200

    start adding course-plan in the server

--- server/users/models.py
+++ server/users/models.py
@@ -5,3 +5,8 @@ from django.db import models
 class User(models.Model):
     email = models.TextField(primary_key=True, blank=False, default='')
     token = models.TextField(blank=True, default='')
+
+class CoursePlan(models.Model):
+    created = models.DateTimeField(auto_now_add=True)
+    title = models.CharField(max_length=100, blank=True, default='')
+    course_codes = models.JSONField(null=True)
--- server/users/serializers.py
+++ server/users/serializers.py
@@ -1,8 +1,13 @@
 from rest_framework import serializers
-from users.models import User
+from users.models import User, CoursePlan
 
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ('email', 'token')
+
+class CoursePlanSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CoursePlan
+        fields = ('id', 'created', 'title', 'course_codes')
