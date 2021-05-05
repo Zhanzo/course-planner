6ea5df26c837b538f5af36bc43b3e7e080c05d29
Robin Andersson <roban591@student.liu.se>
2021-05-05 18:30:42 +0200

    Fix test for courseplans and add a test for courses.

--- server/course_plans/tests.py
+++ server/course_plans/tests.py
@@ -1,6 +1,7 @@
-from django.test import TestCase
 from django.contrib.auth.models import User
-from course_plans.models import CoursePlan
+from django.test import TestCase
+
+from course_plans.models import CoursePlan, Course
 
 
 class UsersManagersTests(TestCase):
@@ -16,9 +17,22 @@ class UsersManagersTests(TestCase):
         user2 = User.objects.create_user(username='normal2')
         self.assertEqual(user2.username, 'normal2')
 
+    def test_create_course(self):
+        course = Course.objects.create(code="TDDB44", name="Compiler Construction", credits=6,
+                                       semester="HT2", level="A1X", module="1")
+        self.assertEqual(course.code, "TDDB44")
+        self.assertEqual(course.name, 'Compiler Construction')
+        self.assertEqual(course.credits, 6)
+        self.assertEqual(course.semester, "HT2")
+        self.assertEqual(course.level, "A1X")
+        self.assertEqual(course.module, "1")
+
     def test_create_course_plan(self):
         user = User.objects.create_user(username='normal')
-        course_plan = CoursePlan.objects.create(owner=user, title="Spring", course_codes={"TDDD27": "Advanced Web"})
+        course = Course.objects.create(code="TDDB44", name="Compiler Construction", credits=6,
+                                       semester="HT2", level="A1X", module="1")
+        course_plan = CoursePlan.objects.create(owner=user, title="Spring")
+        course_plan.courses.add(course)
+        self.assertEqual(course_plan.courses.get(id=course.id), course)
         self.assertEqual(course_plan.owner, user)
         self.assertEqual(course_plan.title, "Spring")
-        self.assertEqual(course_plan.course_codes, {"TDDD27": "Advanced Web"})
