bbd98083fd44e12677e32e72e47b846d6c1708ed
Robin Andersson <roban591@student.liu.se>
2021-05-11 18:04:32 +0200

    Add Facebook sign in on the client and redesign the google sign in button.

--- client/src/app/components/log-in/log-in.component.ts
+++ client/src/app/components/log-in/log-in.component.ts
@@ -1,10 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import {
+  FacebookLoginProvider,
   GoogleLoginProvider,
   SocialAuthService,
   SocialUser,
 } from 'angularx-social-login';
 import { UserService } from 'src/app/services/user.service';
+import { faFacebookF, faGoogle } from '@fortawesome/free-brands-svg-icons';
 
 @Component({
   selector: 'app-log-in',
@@ -14,6 +16,8 @@ import { UserService } from 'src/app/services/user.service';
 export class LogInComponent implements OnInit {
   user?: SocialUser;
   loggedIn?: boolean;
+  faGoogle = faGoogle;
+  faFacebookF = faFacebookF;
 
   constructor(
     private authService: SocialAuthService,
@@ -29,12 +33,15 @@ export class LogInComponent implements OnInit {
 
   signInWithGoogle(): void {
     this.authService.signIn(GoogleLoginProvider.PROVIDER_ID).then(
-      (user) => {
-        this.userService.signIn(user);
-      },
-      (error) => {
-        console.log(error);
-      }
+      (user) => this.userService.signIn(user),
+      (error) => console.log(error)
+    );
+  }
+
+  signInWithFacebook(): void {
+    this.authService.signIn(FacebookLoginProvider.PROVIDER_ID).then(
+      (user) => this.userService.signIn(user),
+      (error) => console.log(error)
     );
   }
 }
