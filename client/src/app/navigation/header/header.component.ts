fae6e1a0719f6a8aa278850928a1b875b9a9c1cd
Maya <somgh007@student.liu.se>
2021-05-31 10:36:33 +0200

    navbar design

--- client/src/app/navigation/header/header.component.ts
+++ client/src/app/navigation/header/header.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { Router } from '@angular/router';
+import { SocialAuthService } from 'angularx-social-login';
+import { LogInComponent } from 'src/app/components/log-in/log-in.component';
 
 @Component({
   selector: 'app-header',
@@ -7,12 +10,29 @@ import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
   @Output() public sidenavToggle = new EventEmitter();
-
+  isloggedIn?: boolean;
   constructor() {}
 
   ngOnInit(): void {}
+  /*
+  constructor(private logincomponent: LogInComponent) {}
 
+  ngOnInit(): void {}
+    if (this.logincomponent.loggedIn) {
+      this.isloggedIn = true;
+    }
+  }
+*/
   public onToggleSidenav = () => {
     this.sidenavToggle.emit();
   };
+  /*
+  signOut(): void {
+    this.authService.signOut().then(() => {
+      localStorage.removeItem('token');
+      localStorage.removeItem('email');
+      this.router.navigateByUrl('home');
+    });
+  }
+*/
 }
